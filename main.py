8fd5e1cf4633b2ba7efb2078ae53ce55c2532f16
Ivan <ravic.ivan88@gmail.com>
2022-07-16 22:04:52 +0200

    Final_1

---
+++ main.py
@@ -0,0 +1,71 @@
+
+from http.client import responses
+from urllib import response
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField
+from wtforms import StringField, SubmitField, TextAreaField, EmailField, PasswordField
+from wtforms.validators import DataRequired, InputRequired
+import os
+from dotenv import load_dotenv, find_dotenv
+import smtplib
+
+import time
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "my_secret_key"
+EMAIL_IVAN_RAVIC = "ravic.ivan88@gmail.com"
+app.config['DEBUG'] = True
+app.config['TESTING'] = False
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TTL'] = True
+app.config['MAIL_USE_SSL'] = False
+# app.config['MAIL_DEBUG'] = True
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_MAX_EMAILS'] = None
+# app.config['MAIL_SUPRESS_SEND'] = False
+app.config['MAIL_ASCII_ATTACHMENT'] = False
+
+# FORM FOR MESSAGE
+class Client_Message(FlaskForm):
+  client_name = StringField("Name", validators = [InputRequired()])
+  client_email = EmailField("Email Address", validators = [InputRequired()])
+  client_message = TextAreaField("Message", validators = [InputRequired()])
+  send = SubmitField("Get In Touch")
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+
+  message_client_form = Client_Message()
+
+  if message_client_form.validate_on_submit():
+        with smtplib.SMTP(app.config['MAIL_SERVER'], port=app.config['MAIL_PORT']) as connection:
+          connection.starttls()
+          connection.login(user=app.config['MAIL_USERNAME'], password=app.config['MAIL_PASSWORD'])
+          connection.sendmail(from_addr=app.config['MAIL_USERNAME'], to_addrs=f"{EMAIL_IVAN_RAVIC}",
+                              msg=f"Subject:{message_client_form.client_name.data}\n\n{message_client_form.client_message.data}\n\nClient email address: {message_client_form.client_email.data}")
+          flash("Message is send. Thanks.")
+        return redirect(url_for("home", _anchor='contact'))
+        
+  return render_template("index.html", message_client_form=message_client_form)
+
+@app.route("/gaginislatkisi", methods=["POST", "GET"])
+def gaginislatkisi():
+  return render_template("project_1_describe.html")
+
+@app.route("/follower_checker", methods=["POST", "GET"])
+def follower_checker():
+  return render_template("project_2_describe.html")
+
+@app.route("/resume_pdf")
+def resume_pdf():
+  return send_from_directory(".", "Ivan's Resume.pdf")
+  
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
